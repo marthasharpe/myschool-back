4175feff6b30cc195f2befb6c769452dca9c0627
Martha Sharpe <marthasharpe2020@gmail.com>
2020-02-03 15:46:23 -0500

    import bodyparser

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 // package for logging requests
 const morgan = require('morgan');
+// package to parse incoming data from the body such as files or JSON
+const bodyParser = require('body-parser');
 
 // import routes
 const subjectRoutes = require('./api/routes/subjects');
 const resourceRoutes = require('./api/routes/resources');
 
+// use morgan logger during development
 app.use(morgan('dev'));;
+// parse url encoded data
+app.use(bodyParser.urlencoded({extended: false}));
+// parse json data
+app.use(bodyParser.json());
 
-// assign middleware for requests
+// use middleware for requests
 app.use('/subjects', subjectRoutes);
 app.use('/resources', resourceRoutes);
 
