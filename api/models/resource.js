fa4c9aac2132a5970af800b7f106b9fa07fb7440
Martha Sharpe <marthasharpe2020@gmail.com>
2020-02-13 12:56:37 -0500

    create resource model and handle get and post requests

---
+++ api/models/resource.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const resourceSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    link: { type: String, required: true },
+    status: { type: String, required: true },
+    subject: { type: String, required: true },
+});
+
+// export name of model and schema of model
+module.exports = mongoose.model('Resource', resourceSchema);
--- api/routes/resources.js
+++ api/routes/resources.js
@@ -1,27 +1,63 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Resource = require('../models/resource')
 
 // handle GET requests to /resources
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling get requests to /resources'
-    })
+    Resource.find()
+        .select('title description _id link status subject')
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                resources: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        title: doc.title,
+                        description: doc.description,
+                        link: doc.link,
+                        status: doc.status,
+                        subject: doc.subject
+                    }
+                })
+            })
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 })
 
 // handle POST requests to /resources
 router.post('/', (req, res, next) => {
     // form of POST request
-    const resource = {
+    const resource = new Resource({
+        _id: mongoose.Types.ObjectId(),
         title: req.body.title,
         description: req.body.description,
         link: req.body.link,
-        subject: req.body.subject,
         status: req.body.status,
-    }
-    res.status(201).json({
-        message: 'Handling post requests to /resources',
-        resource,
+        subject: req.body.subject,
     })
+    resource
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Resource created.',
+                createdResource: {
+                    _id: result._id,
+                    title: result.title,
+                    description: result.description,
+                    link: result.link,
+                    status: result.status,
+                    subject: result.subject,                   
+                }
+            });
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
 })
 
 // handle GET requests to /resources/:resourceId
