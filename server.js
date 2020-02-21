47ad42d39d0497542b452b1975e133f12b328d94
Martha Sharpe <marthasharpe2020@gmail.com>
2020-02-20 19:06:45 -0500

    Procfile

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
 
 const server = http.createServer(app);
 
