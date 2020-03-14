3891a6e936c97c7dadf8c43823318ff7553f819e
Martha Sharpe <marthasharpe2020@gmail.com>
2020-03-13 20:37:13 -0400

    allow url origin only

--- app.js
+++ app.js
@@ -30,17 +30,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // using CORS to allow url origin (enforced by the browser)
-// const whitelist = ['https://my-school.netlify.com/', 'http://localhost:3000']
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//       if (whitelist.indexOf(origin) !== -1) {
-//         callback(null, true)
-//       } else {
-//         callback(new Error('Not allowed by CORS'))
-//       }
-//     }
-//   }
-app.use(cors());
+app.use(cors('https://my-school.netlify.com/'));
 
 // routes which should handle requests
 app.use('/subjects', subjectRoutes);
